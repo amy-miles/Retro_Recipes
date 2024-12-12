6f0e321bcae214004517becb6dae83a618a67613
Amy Miles <33338870+amy-miles@users.noreply.github.com>
2024-12-12 09:08:35 -0600

    removed comment at the top, causes bugs

--- helpers/getRecipeData.php
+++ helpers/getRecipeData.php
@@ -1,9 +1,8 @@
 <?php
 require '../database/db_connect.php';
 
-
 try {
-    // Query the database for the user's recipes
+    // Query the database for all recipe servings data to use globally in app
     $stmt = $conn->prepare("
         SELECT recipe_id, servings, ingredients
         FROM recipes
@@ -13,10 +12,10 @@ try {
 
     $recipes = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Return the recipes as JSON
+    // Return the recipes as JSON object, will be decoded into a PHP object in recipeAdjuctment.js
     header('Content-Type: application/json');
     echo json_encode($recipes);
 } catch (PDOException $e) {
-    http_response_code(500); // Internal Server Error
+    http_response_code(500); 
     echo json_encode(["error" => "Database error: " . $e->getMessage()]);
 }
